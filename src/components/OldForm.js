0084f7122ae9bb5985722b443940f2cfa3346fcd
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-05-23 11:51:31 +0530

    Yup added

--- src/components/OldForm.js
+++ src/components/OldForm.js
@@ -1,78 +1,88 @@
-import React from 'react'
-import { useFormik } from 'formik'
-function OldForm () {
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+function OldForm() {
   const initialValues = {
-    name: '',
-    email: '',
-    channel: ''
-  }
-  const onSubmit = values => console.log('Form data', values)
-  const validate = values => {
-    let errors = {}
+    name: "",
+    email: "",
+    channel: "",
+  };
+  const validationSchema = Yup.object({
+    name: Yup.string().required("Required!!"),
+    email: Yup.string().required("Required!!"),
+    channel: Yup.string().required("Required!!"),
+  });
+  const onSubmit = (values) => console.log("Form data", values);
+  const validate = (values) => {
+    let errors = {};
     if (!values.name) {
-      errors.name = 'Required'
+      errors.name = "Required";
     }
     if (!values.email) {
-      errors.email = 'Required'
+      errors.email = "Required";
     }
     if (!values.channel) {
-      errors.channel = 'Required'
+      errors.channel = "Required";
     }
 
-    return errors
-  }
+    return errors;
+  };
   const formik = useFormik({
     initialValues,
     onSubmit,
-    validate
-  })
+    // validate
+    validationSchema,
+  });
 
   return (
     <div>
       <form onSubmit={formik.handleSubmit}>
-        <div className='form-control'>
-          <label htmlFor='name'>Name</label>
+        <div className="form-control">
+          <label htmlFor="name">Name</label>
           <input
-            type='text'
-            id='name'
-            name='name'
+            type="text"
+            id="name"
+            name="name"
+            onBlur={formik.handleBlur}
             onChange={formik.handleChange}
             value={formik.values.name}
           />
-          {formik.errors.name ? (
-            <div className='error'>{formik.errors.name}</div>
+          {formik.touched.name && formik.errors.name ? (
+            <div className="error">{formik.errors.name}</div>
           ) : null}
         </div>
-        <div className='form-control'>
-          <label htmlFor='email'>E-mail</label>
+        <div className="form-control">
+          <label htmlFor="email">E-mail</label>
           <input
-            type='email'
-            id='email'
-            name='email'
+            type="email"
+            id="email"
+            name="email"
+            onBlur={formik.handleBlur}
             onChange={formik.handleChange}
             value={formik.values.email}
           />
-          {formik.errors.email ? (
-            <div className='error'>{formik.errors.email}</div>
+          {formik.touched.email && formik.errors.email ? (
+            <div className="error">{formik.errors.email}</div>
           ) : null}
         </div>
-        <div className='form-control'>
-          <label htmlFor='channel'>Channel</label>
+        <div className="form-control">
+          <label htmlFor="channel">Channel</label>
           <input
-            type='text'
-            id='channel'
-            name='channel'
+            type="text"
+            id="channel"
+            name="channel"
+            onBlur={formik.handleBlur}
             onChange={formik.handleChange}
             value={formik.values.channel}
           />
-          {formik.errors.channel ? (
-            <div className='error'>{formik.errors.channel}</div>
+          {formik.touched.channel && formik.errors.channel ? (
+            <div className="error">{formik.errors.channel}</div>
           ) : null}
         </div>
-        <button type='submit'>Submit</button>
+        <button type="submit">Submit</button>
       </form>
     </div>
-  )
+  );
 }
 
-export default OldForm
+export default OldForm;
